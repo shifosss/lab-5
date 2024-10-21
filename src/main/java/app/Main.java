f9649aafbffdb9b414c77f8e7b4b3f346c9d9010
Rolland <hexucheng2004@gmail.com>
2024-10-21 17:22:41 -0400

    Rolland He Task2.1 & Task2.2

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -17,6 +17,7 @@ public class Main {
                                             .addLoginView()
                                             .addSignupView()
                                             .addLoggedInView()
+                                            .addLogoutUseCase()
                                             .addSignupUseCase()
                                             .addLoginUseCase()
                                             .addChangePasswordUseCase()
